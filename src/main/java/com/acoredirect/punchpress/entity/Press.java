bcbf9201cad11b1e42ff86923b9b879f76cb2a49
alikskonst <alikskonst@gmail.com>
2019-01-23 19:24:16 +0300

    gitignore was changed, and new Entity model was added

---
+++ src/main/java/com/acoredirect/punchpress/entity/Press.java
@@ -0,0 +1,12 @@
+package com.acoredirect.punchpress.entity;
+
+public class Press {
+
+    private Long id;
+    private String model;
+    private Integer x;
+    private Integer y;
+    private Integer capacity;
+    private String photo;
+    private Boolean exist;
+}
